78cfefcc27c70f33999cf6ecaae76450b9f413ce
stsstt <kingyoulave1@mail.ru>
2025-04-18 18:43:58 +0700

    12

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
 int a;
-        System.out.println("Hellllllllo world!");
+        System.out.println("Helllllo world!");
     }
 }
